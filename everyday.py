6028cef108125d0dc57b3d5532ee4d0040dd8660
Rick Sorensen <rick.sorensen@gmail.com>
2024-02-22 12:00:14 -0600

    everyday: read ds18x20 for outside temperature

--- everyday.py
+++ everyday.py
@@ -1,6 +1,23 @@
 from holiday import Holiday
 import runleds
 import colorsupport
+import onewire
+import ds18x20
+import esp32
+import config
+import machine
+
+
+def get_temp(tmin, tmax, correct=0, tempsens=None):
+    extt = None
+    tmcu = min(max(esp32.mcu_temperature() - correct, tmin), tmax)
+    if tempsens is not None:
+        import time
+
+        tempsens[0].convert_temp()
+        time.sleep_ms(750)
+        extt = min(max(tempsens[0].read_temp(tempsens[1]), tmin), tmax)
+    return (extt, tmcu)
 
 
 def mapRange(value, inMin, inMax, outMin, outMax):
@@ -36,24 +53,43 @@ class Everyday(Holiday):
         self.data = None
         self.tmin = -5
         self.tmax = 35
+        self.tempsens = None
+        try:
+            temppin = config._TEMP_PIN
+            tsens = ds18x20.DS18X20(onewire.OneWire(machine.Pin(temppin)))
+            rs = tsens.scan()
+            if len(rs) == 0:
+                print("no tempsensor found")
+                self.tempsens = None
+            else:
+                self.tempsens = (tsens, rs[0])
+        except:
+            self.tempsens = None
+            print("exception while checking temp sensor")
         # if nrandom is None:
         #    nrandom = len(pix) // 3
         super().__init__(pix, dur=dur, nrandom=nrandom, bright=bright)
 
     def run(self, *, sf=None, choice=None, correct=0, swaprg=True):
-        import esp32
+        try:
+            tout, tmcu = get_temp(self.tmin, self.tmax, correct, tempsens=self.tempsens)
+            t = tout if tout is not None else tmcu
+            # h = (168 - 4.8 * t)/360*65536
+            temphue = mapRange(t, self.tmin, self.tmax, 220, 0)
+            self.data = fadeHue(temphue, nstep=(len(self.pix) // 2 - 10), swaprg=swaprg)
+            nrand = self.nrandom
+        except ImportError:
+            self.data = None
+            t = None
+            nrand = len(self.pix) // 3
 
-        t = min(max(esp32.mcu_temperature() - correct, self.tmin), self.tmax)
-        # h = (168 - 4.8 * t)/360*65536
-        temphue = mapRange(t, self.tmin, self.tmax, 220, 0)
-        self.data = fadeHue(temphue, nstep=(len(self.pix) // 2 - 10), swaprg=swaprg)
-        print("everyday {}+{}".format(t, correct))
+        print("everyday t={}, texternal={}, tmcu={}+{}".format(t, tout, tmcu, correct))
         runleds.loop_led_time(
             self.pix,
             self.data,
             tdur_secs=self.dur,
             sclr=True,
-            nrandom=self.nrandom,
+            nrandom=nrand,
             bright=self.bright,
         )
         return t
