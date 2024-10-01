6d0a8f862a7edf70262ca0d88f80539dbb922b84
Rick Sorensen <rick.sorensen@gmail.com>
2024-10-01 10:41:44 -0500

    initial

---
+++ config.esp32c3.py
@@ -0,0 +1,15 @@
+_IP_ADDR = "192.168.1.174"
+_LONG_DUR = 200
+_TWINK_DUR = 100
+_NUM_PIX = 120
+_HAN_DUR = 100
+_NEOPIN = 2
+_USE_NETWORK = True
+_WAIT_NO_CONNECT = 120  # 7200  # 2 hours
+_USE_DATE = (2024, 10, 31)  # (2024, 12, 10)
+_TEMP_CORRECT = 20
+_SWAPRGB = True
+_TEMP_PIN = 21  # xiao pin 7: esp32c3 GPIO21
+_DEEPSLEEP = False
+_DSLEEP_START = 18.1  # Time to wake from sleep , fractional hour, local STANDARD time
+_RANDOM_RATIO = 10
