2ff5035ef70688027d81cf19c1b283b3d4c22916
Rick Sorensen <rick.sorensen@gmail.com>
2024-10-20 09:04:40 -0500

    neopixel/main.holiday.py: check reason for startup and add delay if not DEEPSLEEP start

--- main.holiday.py
+++ main.holiday.py
@@ -1,6 +1,10 @@
 import os
 
 if "nostart" not in os.listdir():
+    import machine
     import startholiday as sh
 
-    sh.start(delayStart=2, interruptStart=False)
+    delaystart = 0
+    if machine.reset_cause() != machine.DEEPSLEEP_RESET:
+        delaystart = 6
+    sh.start(delayStart=delaystart, interruptStart=False)
