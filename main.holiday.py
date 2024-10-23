bb19055324b2da71ee004d4b6c1d14972cf047ae
Rick Sorensen <rick.sorensen@gmail.com>
2024-10-23 16:29:30 -0500

    neopixel/main.holiday.py: reduce wait time for webrepl

--- main.holiday.py
+++ main.holiday.py
@@ -6,5 +6,5 @@ if "nostart" not in os.listdir():
 
     delaystart = 0
     if machine.reset_cause() != machine.DEEPSLEEP_RESET:
-        delaystart = 6
+        delaystart = 4
     sh.start(delayStart=delaystart, interruptStart=False)
