6246479a6bf6969376db5d7547a54771d7ef7ea5
Rick Sorensen <rick.sorensen@gmail.com>
2024-11-14 21:30:34 -0600

    Clean error reporting;use bitbang

--- startholiday.py
+++ startholiday.py
@@ -9,11 +9,13 @@ import gc
 import config
 import machine
 
+endstat = []
 
 try:
     import esp32
 
     def check_sleep(pix, dosleep=0.25, start=None, stop=23, everydayu=None):
+        global endstat
         dsleept = dosleep if dosleep is not None else 0.25
         dt = holiday.rjslocaltime(tzoff=-6)  # time.localtime()
         hrsleep = int(dsleept * 3600 * 1000)
@@ -27,8 +29,8 @@ try:
         temp = None
         if pix is not None:
             pix.fill((0, 0, 0))
-            if everydayu is not None:
-                c, temp = everydayu.getTempColor(b=0.3)
+            if ((hrnow < 8.5) or (hrnow > 15.5)) and (everydayu is not None):
+                c, temp = everydayu.getTempColor(b=0.1)
                 print("Setting day temp  ", c)
                 lp = len(pix) - 60
                 for i in range(lp, len(pix)):
@@ -39,6 +41,7 @@ try:
                 pix.write()
         if (dosleep is not None) and (hrsleep > 0):
             print(f"deepsleep active {hrsleep} {temp}")
+            endstat.append("deepsleep active")
             mqttquick.msgalert(hrsleep, hrnow, temp=temp, addtopic="x")
             time.sleep(0.2)
             machine.deepsleep(hrsleep)
@@ -47,7 +50,7 @@ try:
             return hrsleep
 
     # esp32.RMT.bitstream_channel(0)  # default is 1
-    # esp32.RMT.bitstream_channel(None)  # use bitbanging
+    esp32.RMT.bitstream_channel(None)  # use bitbanging
     haveTemp = True
 except ImportError:
     haveTemp = False
@@ -61,116 +64,140 @@ if config._USE_NETWORK:
     import ntptime
 
 
-endstat = []
-
-
 def start(interruptStart=True, delayStart=0, force_date=None, fixtemp=None):
+    global endstat
     if interruptStart:
         print("time start up interrupt")
         time.sleep(60)
-    allokay = True
+    allokay = False
     if config._USE_NETWORK:
         print("starting webrepl ", config._IP_ADDR)
         allokay = netconnect.dowrepl(myIP=config._IP_ADDR)
         # allokay = netconnect.doviperide(myIP=config._IP_ADDR)
         print("net status: ", allokay)
-        while delayStart > 0 and os.dupterm(None) is None:
+        while allokay and (delayStart > 0) and (os.dupterm(None) is None):
             print("wait for WebREPL connection")
             time.sleep(30)
             delayStart = delayStart - 1
+    else:
+        allokay = True
+        endstat.append("no network requested")
+    endstat.append(f"network allok:{allokay}")
     print("starting lights")
     print("initial memory ", gc.mem_free())
     if allokay:
-        if force_date is None and config._USE_DATE is None:
-            if config._USE_NETWORK:
-                time.sleep(5)
-                ntptime.settime()
-            dt = holiday.rjslocaltime(tzoff=-6)
-        else:
-            import machine
-
-            r = machine.RTC()
-            if force_date is None:
-                force_date = config._USE_DATE
-            r.datetime(
-                (
-                    force_date[0],
-                    force_date[1],
-                    force_date[2],
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                )
-            )
-            dt = time.localtime()
-        print(dt)
-        hardsleep = config._DEEPSLEEP  # should read from config.py
-        stoptime = config._DSLEEP_STOP if hasattr(config, "_DSLEEP_STOP") else 23
-        pix = runleds.test_setup(
-            config._NUM_PIX, pin=config._NEOPIN, swaprgb=config._SWAPRGB
-        )
-        if haveTemp:
-            tmcu = esp32.mcu_temperature()
-            tmcu = esp32.mcu_temperature()
-            print("temp ", tmcu)
-        else:
-            print("no temperature available")
-
-        hanukkah = holiday.Hanukkah(
-            pix,
-            dur=config._HAN_DUR,
-            nrandom=(
-                (len(pix) // config._RANDOM_RATIO)
-                if config._RANDOM_RATIO is not None
-                else None
-            ),
-        )
-        valentine = holiday.Valentine(
-            pix,
-            dur=config._HAN_DUR,
-            nrandom=None,
-        )
-        stpats = holiday.SaintPatrick(
-            pix,
-            dur=config._HAN_DUR,
-            nrandom=None,
-        )
-        christmas = holiday.Christmas(
-            pix,
-            dur=config._LONG_DUR,
-            nrandom=(
-                (len(pix) // config._RANDOM_RATIO)
-                if config._RANDOM_RATIO is not None
-                else None
-            ),
-        )
-        birthday = holiday.Birthday(pix, dur=config._LONG_DUR)
-        halloeve = halloween.Halloween(pix)
-        fallback = everyday.Everyday(
-            pix,
-            dur=config._LONG_DUR,
-            fixtemp=fixtemp,
-            nrandom=(
-                (len(pix) // config._RANDOM_RATIO)
-                if config._RANDOM_RATIO is not None
-                else None
-            ),
-        )
-        if (
-            check_sleep(
-                pix,
-                dosleep=hardsleep,
-                start=config._DSLEEP_START,
-                stop=stoptime,
-                everydayu=fallback,
-            )
-            > 0
-        ):
-            time.sleep(10)
-        print(" Allocate memory :", gc.mem_free())
         try:
+            pix = runleds.test_setup(
+                config._NUM_PIX, pin=config._NEOPIN, swaprgb=config._SWAPRGB
+            )
+            if force_date is None and config._USE_DATE is None:
+                if config._USE_NETWORK:
+                    time.sleep(5)
+                    retry = 5
+                    while retry > 0:
+                        retry = retry - 1
+                        ntptime.timeout = 5
+                        try:
+                            ntptime.settime()
+                            retry = 0
+                            endstat.append("set ntp date")
+                        except Exception as ntpexcept:
+                            print("ntp timeout")
+                            endstat.append(f"ntp timeout {retry}")
+                            endstat.append(ntpexcept)
+                            if retry == 0:
+                                esp32.RMT.bitstream_channel(1)
+                                pix.fill((0, 0, 0))
+                                pix.write()
+                                raise ntpexcept
+                dt = holiday.rjslocaltime(tzoff=-6)
+                endstat.append(f"set date {dt}")
+
+            else:
+                import machine
+
+                r = machine.RTC()
+                if force_date is None:
+                    force_date = config._USE_DATE
+                r.datetime(
+                    (
+                        force_date[0],
+                        force_date[1],
+                        force_date[2],
+                        0,
+                        0,
+                        0,
+                        0,
+                        0,
+                    )
+                )
+                dt = time.localtime()
+                endstat.append("RTC time used")
+            endstat.append(f"date: {dt}")
+            print(dt)
+            hardsleep = config._DEEPSLEEP  # should read from config.py
+            stoptime = config._DSLEEP_STOP if hasattr(config, "_DSLEEP_STOP") else 23
+            if haveTemp:
+                tmcu = esp32.mcu_temperature()
+                tmcu = esp32.mcu_temperature()
+                print("temp ", tmcu)
+            else:
+                print("no temperature available")
+
+            hanukkah = holiday.Hanukkah(
+                pix,
+                dur=config._HAN_DUR,
+                nrandom=(
+                    (len(pix) // config._RANDOM_RATIO)
+                    if config._RANDOM_RATIO is not None
+                    else None
+                ),
+            )
+            valentine = holiday.Valentine(
+                pix,
+                dur=config._HAN_DUR,
+                nrandom=None,
+            )
+            stpats = holiday.SaintPatrick(
+                pix,
+                dur=config._HAN_DUR,
+                nrandom=None,
+            )
+            christmas = holiday.Christmas(
+                pix,
+                dur=config._LONG_DUR,
+                nrandom=(
+                    (len(pix) // config._RANDOM_RATIO)
+                    if config._RANDOM_RATIO is not None
+                    else None
+                ),
+            )
+            birthday = holiday.Birthday(pix, dur=config._LONG_DUR)
+            halloeve = halloween.Halloween(pix)
+            fallback = everyday.Everyday(
+                pix,
+                dur=config._LONG_DUR,
+                fixtemp=fixtemp,
+                nrandom=(
+                    (len(pix) // config._RANDOM_RATIO)
+                    if config._RANDOM_RATIO is not None
+                    else None
+                ),
+            )
+            if (
+                check_sleep(
+                    pix,
+                    dosleep=hardsleep,
+                    start=config._DSLEEP_START,
+                    stop=stoptime,
+                    everydayu=fallback,
+                )
+                > 0
+            ):
+
+                time.sleep(10)
+            print(" Allocate memory :", gc.mem_free())
+            endstat.append("holidays created")
             while True:
                 didholiday = (
                     birthday.chkDate(dt=dt, run=True)
@@ -204,6 +231,9 @@ def start(interruptStart=True, delayStart=0, force_date=None, fixtemp=None):
             endstat.append("Unexpected Exception")
             endstat.append(unexpected)
             sys.print_exception(unexpected)
+            esp32.RMT.bitstream_channel(1)
+            pix.fill((0, 0, 0))
+            pix.write()
     else:
         print("Not All OKAY")
         endstat.append("Not ALL OKAY")
