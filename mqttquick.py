847c076f9912ef296d08dbe29adea34e80c6bf69
Rick Sorensen <rick.sorensen@gmail.com>
2025-03-16 17:36:46 -0500

    neopixel: Add mqtt 'start' command
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- mqttquick.py
+++ mqttquick.py
@@ -34,28 +34,45 @@ def getstart_time(start):
 
 
 _dostop = False
+_newstart = False
 
 
 def _sub_cb(topic, msg):
     global _dostop
+    global _newstart
     _dostop = b"stop" in msg
+    _newstart = b"start" in msg
+    # print(topic + " sub_cb " + msg)
 
 
 # remember to send message as retained
-def checkstop(topic=b"alert/control"):
+def checkcontrol(topic=b"alert/control"):
+    global _newstart
     global _dostop
-    _dostop = False
     mqttc = MQTTClient("esp32c3xiaoUniq", "192.168.1.88")
     mqttc.set_callback(_sub_cb)
     mqttc.connect()
     mqttc.subscribe(topic)
     ts = time.ticks_ms()
+    dostp = False
+    dostr = False
     while time.ticks_diff(time.ticks_ms(), ts) < 3000:
         mqttc.check_msg()
-        if _dostop:
+        if _newstart or _dostop:
             # reset message
+            # mqttc.set_callback(None)
+            dostr = _newstart
+            dostp = _dostop
             mqttc.publish(topic, b"okay", retain=True)
             break
-        time.sleep(1)
+        time.sleep(0.5)
     mqttc.disconnect()
-    return _dostop
+    # print(f"checkcontrol stop={dostp}  start={dostr}")
+    return dostp, dostr
+
+
+def sendmsg(msg, topic=b"alert/message"):
+    mqttc = MQTTClient("esp32c3xiaoUniq", "192.168.1.88", keepalive=60)
+    mqttc.connect()
+    mqttc.publish(topic, msg, retain=True)
+    mqttc.disconnect()
