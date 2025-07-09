9356470bf40dcd62a8ec04ed798ab86b933f8a08
Rick Sorensen <rick.sorensen@gmail.com>
2025-07-08 19:50:28 -0500

    neopixl/fire: update
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- fire.py
+++ fire.py
@@ -3,6 +3,7 @@ import effect_panel
 import gc
 import time
 import random
+import boom
 
 
 # duration in milliseconds
@@ -16,6 +17,7 @@ class Fire(holiday.Holiday):
         update=10,
         nrandom=None,
         top=True,
+        fw=None,
         bright=0.1,
         debug=False,
     ):
@@ -23,6 +25,7 @@ class Fire(holiday.Holiday):
         self.update = update
         self.dur = dur
         self.top = top
+        self.fw = fw
         super().__init__(pix, dur=dur, nrandom=nrandom, bright=bright)
 
     def chkDate(self, dt=None, run=False, bright=None):
@@ -37,34 +40,42 @@ class Fire(holiday.Holiday):
     def run(self, *, choice=None, bright=None):
         if bright is None:
             bright = self.bright
+        # True is fire on top, else on bottom (inverted for flag)
         firetop = random.choice([True, False]) if self.top is None else self.top
-        print(f"fire {self.dur} {self.update} {firetop}")
-        gc.collect()
-        panel = effect_panel.effect_panel(
-            self.pix, 36, 2 if len(self.pix) < 200 else 6, ledblock=5
-        )
-        nflgiter = 2 if len(self.pix) < 200 else 3
-        tstart = time.ticks_ms()
-        ct = 0
-        while time.ticks_diff(time.ticks_ms(), tstart) < self.dur:
-            panel.firelight(
-                brightness=min(int(bright * 255), 255),
-                red=255,
-                green=64,
-                blue=10,
-                speed=128,
-                fade=255,
-                top=firetop,
-                debug=self.debug,
+        # True is boom, else fire & flag
+        effect_op = random.choice([True, False]) if self.fw is None else not self.fw
+        print(f"fire {self.dur} {self.update} {firetop} {effect_op}")
+        if effect_op:
+            gc.collect()
+            panel = effect_panel.effect_panel(
+                self.pix, 36, 2 if len(self.pix) < 200 else 6, ledblock=5
             )
-            if ct == 0:
-                panel.flag(
-                    top=not firetop,
-                    niter=nflgiter,
+            nflgiter = 2 if len(self.pix) < 200 else 3
+            tstart = time.ticks_ms()
+            ct = 0
+            # do fire with flag
+            while time.ticks_diff(time.ticks_ms(), tstart) < self.dur:
+                panel.firelight(
                     brightness=min(int(bright * 255), 255),
+                    red=255,
+                    green=64,
+                    blue=10,
+                    speed=128,
+                    fade=255,
+                    top=firetop,
                     debug=self.debug,
                 )
-            ct = (ct + 1) % 4
-            panel.update()
-            time.sleep_ms(self.update)
+                if ct == 0:
+                    panel.flag(
+                        top=not firetop,
+                        niter=nflgiter,
+                        brightness=min(int(bright * 255), 255),
+                        debug=self.debug,
+                    )
+                ct = (ct + 1) % 4
+                panel.update()
+                time.sleep_ms(self.update)
+        else:
+            # to fire work
+            boom.doall(self.pix, durms=self.dur, brightness=bright, dly=self.update)
         gc.collect()
