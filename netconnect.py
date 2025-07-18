6f51c0bede901beba9888cba1ef18c20e36c7311
Rick Sorensen <rick.sorensen@gmail.com>
2025-07-18 13:31:58 -0500

    neopixel/netconnect: search for wifi host with best strength
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- netconnect.py
+++ netconnect.py
@@ -1,6 +1,21 @@
 import time
 
 
+def chkssid(wlan):
+    wlan.active(True)
+    # acthost={}
+    hosts = wlan.scan()
+    maxstrength = [-1000, "NONET"]
+    for h in hosts:
+        if "RJS" in h[0]:
+            # acthosts[h[0]] = h[3]
+            if h[3] > maxstrength[0]:
+                maxstrength[0] = h[3]
+                maxstrength[1] = h[0]
+    print(f"chkssid: {maxstrength}")
+    return maxstrength[1]
+
+
 def connect(ssid="RJSNG_24", key="december23", timeout=30000):
     import network
 
@@ -8,6 +23,8 @@ def connect(ssid="RJSNG_24", key="december23", timeout=30000):
     tout = False
     if not sta_if.isconnected():
         print("restarting network connection")
+        if ssid is None:
+            ssid = chkssid(sta_if)
         sta_if.active(True)
         sta_if.connect(ssid, key)
         tstart = time.ticks_ms()
@@ -17,10 +34,10 @@ def connect(ssid="RJSNG_24", key="december23", timeout=30000):
                 break
             time.sleep(1)  # wait till connection
     if tout:
-        print("network not connected")
+        print(f"network {ssid} not connected")
         sta_if.disconnect()
     else:
-        print("network config:", sta_if.ifconfig())
+        print(f"network {ssid} config:", sta_if.ifconfig())
 
 
 def connectIP(ssid="RJSNG_24", key="december23", timeout=30000, myIP="192.168.1.177"):
@@ -36,6 +53,8 @@ def connectIP(ssid="RJSNG_24", key="december23", timeout=30000, myIP="192.168.1.
         sta_if.active(True)
         if myIP is not None:
             sta_if.ifconfig((myIP, "255.255.255.0", "192.168.1.18", "8.8.8.8"))
+        if ssid is None:
+            ssid = chkssid(sta_if)
         sta_if.connect(ssid, key)
         tstart = time.ticks_ms()
         while not sta_if.isconnected():
@@ -43,22 +62,25 @@ def connectIP(ssid="RJSNG_24", key="december23", timeout=30000, myIP="192.168.1.
                 tout = True
                 break
             time.sleep(1)  # wait till connection
+        with open("usedssid", "w") as f:
+            f.write(f"used SSID={ssid}  connected={not tout}")
+        print(f"used SSID={ssid}  connected={not tout}")
     elif myIP is not None and sta_if.ifconfig()[0] != myIP:
         print("resetting IP address from {} to {}".format(sta_if.ifconfig()[0], myIP))
         sta_if.ifconfig((myIP, "255.255.255.0", "192.168.1.18", "8.8.8.8"))
     if tout:
-        print("network not connected")
+        print(f"network {ssid} not connected")
         sta_if.disconnect()
     else:
-        print("network config:", sta_if.ifconfig())
+        print(f"network {ssid} config:", sta_if.ifconfig())
     return not tout
 
 
-def dowrepl(myIP="192.168.1.174"):
+def dowrepl(ssid="RJSNG_24", key="december23", myIP="192.168.1.174"):
     import webrepl
 
     print("dowrepl ", myIP)
-    rv = connectIP(myIP=myIP)
+    rv = connectIP(ssid=ssid, key=key, myIP=myIP)
     if rv:
         webrepl.start()
     return rv
