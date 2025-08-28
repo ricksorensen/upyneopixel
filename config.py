bbc5ac92292122882a2ee653ab918c60c2c31710
Rick Sorensen <rick.sorensen@gmail.com>
2025-08-28 14:42:47 -0500

    config: for mpremote local mount
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- config.py
+++ config.py
@@ -3,15 +3,16 @@ _LONG_DUR = 15
 _TWINK_DUR = 15
 _NUM_PIX = 120
 _HAN_DUR = 15
-_NEOPIN = 2
+_NEOPIN = 1  # 2 for C3, 1 for S3
 _USE_NETWORK = True
 _WAIT_NO_CONNECT = 30  # 7200  # 2 hours
 _USE_DATE = None  # (2025, 4, 1)  # (2024, 10, 31)  # (2024, 12, 10)
 _TEMP_CORRECT = 20
 _SWAPRGB = False
-_TEMP_PIN = 21  # xiao pin 7: esp32c3 GPIO21
-_LDR_PIN = 4  # xiao pin 2: esp32c3 GPIO04
+_TEMP_PIN = 43  # xiao pin 7: esp32c3 GPIO21   esp32s3: GPIO43
+_LDR_PIN = 3  # xiao pin 2: esp32c3 GPIO04 GPIO3 3 for S3
 _LDR_DARKUV = 1500000  # microvolt to define on dark level
+_LDR_TURNON = 2000
 _LDR_REPORT = True
 
 # If True, use LDR to determine on/off
@@ -26,7 +27,10 @@ _DSLEEP_STOP = 23.5
 _RANDOM_RATIO = None
 _USEBITBANG = False
 _DEBUG = True
+_LOGLEVEL = "warn"  # error, warn, info, debug default is error
 _SUFFIX = "test"
 
 # effects
 _FIRETOP = None
+_EVERYDAY_OPT = "FF, TEMP, RAND, FWORK"
+# _EVERYDAY_OPT = "FWORK"
