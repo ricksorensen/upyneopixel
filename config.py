52b5a52ed0387e670020e1fa3ffc022ba6b5237c
Rick Sorensen <rick.sorensen@gmail.com>
2026-06-29 14:40:55 -0500

    config: Add _FLOW_DELAY.
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 _IP_ADDR = "192.168.1.177"
-_LONG_DUR = 15
+_LONG_DUR = 30
 _TWINK_DUR = 15
 _NUM_PIX = 120
-_HAN_DUR = 15
-_NEOPIN = 1  # 2 for C3, 1 for S3
+_HAN_DUR = 30
+_NEOPIN = 2  # 2 for C3, 1 for S3
 _USE_NETWORK = True
 _WAIT_NO_CONNECT = 30  # 7200  # 2 hours
 _USE_DATE = None  # (2025, 4, 1)  # (2024, 10, 31)  # (2024, 12, 10)
 _TEMP_CORRECT = 20
 _SWAPRGB = False
-_TEMP_PIN = 43  # xiao pin 7: esp32c3 GPIO21   esp32s3: GPIO43
-_LDR_PIN = 3  # xiao pin 2: esp32c3 GPIO04 GPIO3 3 for S3
+_TEMP_PIN = 21  # xiao pin 7: esp32c3 GPIO21   esp32s3: GPIO43
+_LDR_PIN = 3  # xiao pin 3: esp32c3 GPIO04 GPIO3 3 for S3
 _LDR_DARKUV = 1500000  # microvolt to define on dark level
 _LDR_TURNON = 2000
 _LDR_REPORT = True
-
+_LDR_LIGHTNING = 2200000
 # If True, use LDR to determine on/off
 #    False, use TOD to determing on/off
 _DAYNIGHT_ON = False
@@ -27,7 +27,7 @@ _DSLEEP_STOP = 23.5
 _RANDOM_RATIO = None
 _USEBITBANG = False
 _DEBUG = True
-_LOGLEVEL = "warn"  # error, warn, info, debug default is error
+_LOGLEVEL = "info"  # error, warn, info, debug default is error
 _SUFFIX = "test"
 
 # effects
@@ -37,7 +37,9 @@ _SUFFIX = "test"
 # TEMP: show temp
 # FWORK: use fireworks
 _FIRETOP = None
-_EVERYDAY_OPT = "FF, RAND"
+# _EVERYDAY_OPT = "FF, RAND, LIGHTNING"
+_EVERYDAY_OPT = "RAND"
+_FLOW_DELAY = 0.02
 _CHRISTMAS_OPT = "FFNUM=5, TWINKLE, STREAM"
 # _EVERYDAY_OPT = "FWORK"
 
